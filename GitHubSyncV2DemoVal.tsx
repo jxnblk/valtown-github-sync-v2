25b6413216c2052932babde00afcd2b8e7005c3d
Brent Jackson <jxnblk@gmail.com>
2025-02-07 15:06:34 -0500

    Revert

--- GitHubSyncV2DemoVal.tsx
+++ GitHubSyncV2DemoVal.tsx
@@ -1,4 +1,23 @@
 /** Demo val for GitHub sync */
+const html = `
+<html>
+  <head>
+    <title>GitHubSync test</title>
+  </head>
+  <body>
+    <h1>GitHubSync Test</h1>
+    <p>
+      I edited this in vim, then pushed it to GitHub
+    </p>
+  </body>
+</html>
+`;
+
 export default async function (req: Request): Promise<Response> {
-  return Response.json({ ok: true })
+  console.log("GitHubSyncV2DemoVal", import.meta.url);
+  return new Response(html, {
+    headers: {
+      "Content-Type": "text/html",
+    },
+  });
 }
